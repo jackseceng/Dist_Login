465fa4e207bab4290456e5d817b9bf147a245284
Jack Edwards <34158175+JEdwards27@users.noreply.github.com>
2019-03-05 20:26:27 +0000

    Update server_ssl_setup.sh
    Added suggestion to use USB storage device to transfer certificates to client. Working on feature that automates the renaming and transfer of certificates to and from the USB device upon initialisation of the KEYDRIVE using the new user mode in the main program.

--- Files/ssl/server_ssl_setup.sh
+++ Files/ssl/server_ssl_setup.sh
@@ -4,5 +4,5 @@ openssl rsa -in server.orig.key -out server.key
 openssl -req -new -key server.key -out server.csr
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 echo "if [[ -n $SSH_CONNECTION ]] ; then python3.7 /home/user/Dist_Login/Libraries/dist_login.py fi" >> ~/.bashrc
-echo "Now copy server.crt to the client in the '<path_to_Dist_Login_upper_directory>/Dist_Login/Files/ssl/' directory, and rename it to send_server.crt"
+echo "Now copy server.crt to the client in the '<path_to_Dist_Login_upper_directory>/Dist_Login/Files/ssl/' directory, and rename it to send_server.crt (It is recommended to transfer the certificates via a USB storage device)"
 echo "Then run 'Python3.7 <path_to_Libraries_directory>/recv.py' in a separate terminal and leave it running"
