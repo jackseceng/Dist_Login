6d965b75c2f7c857c530c9434c21d1ceef86ce99
JEdwards <Zyr$xq7IUQAx>
2019-03-12 17:29:57 +0000

    Changed a few variables names

--- Libraries/client_recv.py
+++ Libraries/client_recv.py
@@ -7,25 +7,24 @@ def save_data(data):
     with f:
         f.write(received)
 
-def receive_data(connstream):
-    data = connstream.read()
+def receive_data(stream):
+    data = stream.read()
     while data:
         if not save_data(data):
             break
-        data = connstream.read()
+        data = stream.read()
 
 bindsocket = socket.socket()
-bindsocket.bind(('', <client_port))
+bindsocket.bind(('', <client_port>))
 bindsocket.listen(5)
 
 while True:
     newsocket, fromaddr = bindsocket.accept()
-    connstream = ssl.wrap_socket(newsocket, server_side = True, certfile = "<path_to_Dist_Login>/Files/ssl/recv_server.crt", keyfile = "<Path_to_Dist_Login>/Files/ssl/recv_server.key")
+    stream = ssl.wrap_socket(newsocket, server_side = True, certfile = "<path_to_Dist_Login>/Files/ssl/recv_server.crt", keyfile = "<Path_to_Dist_Login>/Files/ssl/recv_server.key")
     try:
         print('Receiving usb key chunk')
-        receive_data(connstream)
+        receive_data(stream)
     finally:
-        connstream.shutdown(socket.SHUT_RDWR)
-        connstream.close()
+        stream.shutdown(socket.SHUT_RDWR)
+        stream.close()
         print("Closed local file")
-
